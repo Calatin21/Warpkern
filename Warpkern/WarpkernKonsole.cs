8629bbce8d15a87d31d4c0d3a6b345500f67590c
Calatin21 <oshaughnessy@hotmail.de>
2023-04-27 12:17:46 +0200

    patch

--- Warpkern/WarpkernKonsole.cs
+++ Warpkern/WarpkernKonsole.cs
@@ -1,10 +1,15 @@
 ﻿namespace Warpkern {
     internal class WarpkernKonsole {
         public void Überhitzt(object source, WarpeventArgs e) {
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Warpkern Temperatur über 500 Grad!");
-            Console.ForegroundColor = ConsoleColor.Gray;
+            for (int i = 0; i < 2; i++) {
+               Console.Clear();
+                Console.WriteLine("Warpkern Temperatur über 500 Grad!");
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Warpkern Temperatur über 500 Grad!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Thread.Sleep(30);
+            }
         }
         public void TemperaturVeraenderung(object source, WarpeventArgs e) {
             if (e.Temperatur <= 500) {
@@ -15,10 +20,25 @@
             Console.WriteLine("Warpkern neue Temperatur: {0}", e.Temperatur);
         }
         public void Kernschmelze(object source, WarpeventArgs e) {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
-            Console.WriteLine("Totale Katastrophe der Warpkern ist geschmolzen und die Enterprise ist zerstört!");
-            System.Environment.Exit(0);
+            bool blink = false;
+            while (true) {
+                Console.Clear();
+                if (blink) {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Totale Katastrophe der Warpkern ist geschmolzen und die Enterprise ist zerstört!");
+                    blink = false;
+                    Thread.Sleep(100);
+                }
+                else {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Totale Katastrophe der Warpkern ist geschmolzen und die Enterprise ist zerstört!");
+                    blink = true;
+                    Thread.Sleep(400);
+
+                }
+            }
         }
     }
 }
